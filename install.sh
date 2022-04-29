d60336aa81458786520cc8dbfa72e6ba5ee9e184
Kylie Stradley <4666485+KyFaSt@users.noreply.github.com>
2022-04-29 19:23:06 +0000

    please let this be the one that works

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ setup_homebrew() {
     if test ! "$(command -v brew)"; then
         info "Homebrew not installed. Installing."
         # Run as a login shell (non-interactive) so that the script doesn't pause for user input
-        curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 }
 
