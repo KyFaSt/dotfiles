54458380c86fce0c478dbb78f30c1e14db3bc48e
Kylie Stradley <4666485+KyFaSt@users.noreply.github.com>
2022-04-29 17:41:38 +0000

    try to install homebrew

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ setup_homebrew() {
     if test ! "$(command -v brew)"; then
         info "Homebrew not installed. Installing."
         # Run as a login shell (non-interactive) so that the script doesn't pause for user input
-        curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+        curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
     fi
 }
