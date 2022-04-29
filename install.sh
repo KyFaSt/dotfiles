fcce70cde216e7e3a01794bd76ac1b423530bdbb
Kylie Stradley <4666485+KyFaSt@users.noreply.github.com>
2022-04-29 22:07:27 +0000

    remove bash from install and call brew bundle after installing it

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 setup_homebrew() {
-    title "Setting up Homebrew"
+  echo "setting up homebrew"
 
     if test ! "$(command -v brew)"; then
-        info "Homebrew not installed. Installing."
+        echo "Homebrew not installed. Installing."
         # Run as a login shell (non-interactive) so that the script doesn't pause for user input
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
-
-    brew bundle
 }
 
 setup_homebrew
+brew bundle
